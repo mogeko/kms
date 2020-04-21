f71feb05f457afa64947fd36dec2d66a180a5592
Mogeko <zhengjunyi@live.com>
2020-04-22 00:15:05 +0200

    Convert Office (Retail) to Office (VOL) Only supports Office 2016+ issue #4

--- run.ps1
+++ run.ps1
@@ -14,6 +14,7 @@ $script:LICENCE = @"
 "@
 $script:KMS_SERVER = 'kms.mogeko.me'
 $script:DATA_JSON_URL = 'https://mogeko.github.io/kms/data.json'
+# $script:DATA_JSON_URL = 'http://localhost:8000/data.json' # TEST
 
 if (Test-Path .\data.json) {
     $script:MENU_INFO = Get-Content .\data.json | ConvertFrom-Json
@@ -77,6 +78,8 @@ function Main {
                 ActivateWindows $main.option[$inputItem-49].name;
             } elseif ($main.option[$inputItem-49].action -eq "ActivateOffice") {
                 ActivateOffice $main.Option[$inputItem-49].name $main.option[$inputItem-49].key $main.option[$inputItem-49].osppdir;
+            } elseif ($main.option[$inputItem-49].action -eq "ConvertOffice") {
+                ConvertOffice $main.Option[$inputItem-49].name $main.option[$inputItem-49].osppdir $main.option[$inputItem-49].licenses;
             }
         }
     }
@@ -142,17 +145,44 @@ function ActivateOffice {
     Write-Host $WARNING -f red
     Invoke-Expression 'Pause'
 
-    Invoke-Expression "$cmd '$ospp' /inpkey:$key"
-    Invoke-Expression "$cmd '$ospp' /sethst:$script:KMS_SERVER"
-    Invoke-Expression "$cmd '$ospp' /act"
+    Invoke-Expression "$cmd `"$ospp`" /inpkey:$key"
+    Invoke-Expression "$cmd `"$ospp`" /sethst:$script:KMS_SERVER"
+    Invoke-Expression "$cmd `"$ospp`" /act"
 
     Write-Host "$nl"
     Write-Host "激活完成！！"-f green
     Write-Host "当前 $name 的激活状态:" -f green
     Write-Host "$nl"
-    Invoke-Expression "$cmd '$ospp' /dstatus"
+    Invoke-Expression "$cmd `"$ospp`" /dstatus"
+    Invoke-Expression 'Pause'
+}
+
+function ConvertOffice {
+    param (
+        [String]$name, [String]$osppPath, [Array]$licenses
+    )
+    $cmd = 'cscript'
+    $ospp = CheckOSPP $osppPath
+    $WARNING = @"
+作者用爱发电，出了任何问题都是不会负责的
+回车后就没有回头路了，你是否真的想好了？
+如果 ospp.vbs 位置不对请立即 Ctrl+C 强制退出
+"@
+    Invoke-Expression 'cls';
+    "正在{0}..." -f $name
+    "ospp.vbs 位置: {0}" -f $ospp;
+    foreach ($licitem in $licenses) {
+        "license 位置(相对于 ospp.vbs): ..\root\{0}" -f $licitem
+    }
+    Write-Host $WARNING -f red
     Invoke-Expression 'Pause'
 
+    # cscript "%ospp%" /inslic:"..\root\Licenses16\ProPlusVL_KMS_Client-ppd.xrm-ms"
+    foreach ($licitem in $licenses) {
+        Write-Host "$cmd `"$ospp`" /inslic:`"..\root\$licitem`""
+        # Invoke-Expression "$cmd `"$ospp`" /inslic:$licitem"
+    }
+    Invoke-Expression 'Pause'
 }
 
 Main $script:MENU_INFO
